74f52a0dc55f679dbe91c9c4260ab4b0bf4fbead
offontime <114061237+offontime@users.noreply.github.com>
2026-03-10 11:23:17 +0800

    Add /usb/multi-macintosh page

---
+++ src/app/usb/multi-macintosh/page.tsx
@@ -0,0 +1,13 @@
+import './page.scss';
+
+export default function UsbMultiMacintosh() {
+ return (
+    <div className="usb-macintosh-container">
+      <div className="content">
+        <h1>USB Multi-Macintosh</h1>
+        <p>正在开发</p>
+        <p>Under Development</p>
+      </div>
+    </div>
+  );
+}
