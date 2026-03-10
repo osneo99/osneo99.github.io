07199f7661df656b215228a743217bc06f7bc0ea
offontime <114061237+offontime@users.noreply.github.com>
2026-03-10 11:40:54 +0800

    update text

--- src/app/usb/multi-macintosh/page.tsx
+++ src/app/usb/multi-macintosh/page.tsx
@@ -6,7 +6,7 @@ export default function UsbMultiMacintosh() {
       <div className="content">
         <h1>MacOS 多版本集合</h1>
         <p>正在开发</p>
-        <p>Under Development</p>
+        <p>这里会做一个这个多版本集合安装 u盘的介绍和购买联系方式</p>
       </div>
     </div>
   );
